9577d365151dc1ba9add4b7ae960f011b181f0fb
Alan <alan@MacBook-Pro-de-Alan.local>
2025-05-06 09:03:37 -0300

    dist url

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use("/api/products", productRoutes);
 
 // Serve frontend apenas em produção
 if (process.env.NODE_ENV === "production") {
-  const frontendPath = path.join(projectRoot, "frontend", "dist");
+  const frontendPath = path.join(__dirname, "frontend", "dist");
 
   app.use(express.static(frontendPath));
 
